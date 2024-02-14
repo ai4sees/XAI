ac5acc292e36dc16f0fb96d9c02ac42d63595306
ahmednadiyah1 <104296674+ahmednadiyah1@users.noreply.github.com>
2024-02-15 00:41:09 +0530

    Update preprocess.py
    updated preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -6,7 +6,7 @@ from io import BytesIO
 from zipfile import ZipFile
 from urllib.request import urlopen
 import dill
-
+import argparse
 
 
 
@@ -15,10 +15,21 @@ def preprocess_data(df, normalize=True, standardize=False):
     df_temp = df.copy()
     df_temp = df_temp.dropna()
     df_temp = df_temp.reset_index()
-    
-    df_temp = df_temp.drop(['index', 'No', 'day', 'month', 'year', 'hour', 'PM10'], axis=1)
-    df_temp['wd'] = df_temp['wd'].astype('category').cat.codes
-    df_temp['station'] = df_temp['station'].astype('category').cat.codes
+
+    col = ['index', 'No', 'day', 'month', 'year', 'hour', 'PM10']
+    for i in col:
+        if i in df_temp.columns:
+            df_temp.drop(i, axis=1, inplace = True)
+
+    if "wd" in df_temp.columns:
+        df_temp['wd'] = df_temp['wd'].astype('category').cat.codes
+
+    if "cbwd" in df_temp.columns:
+        df_temp['cbwd'] = df_temp['cbwd'].astype('category').cat.codes
+
+
+    if "station" in df_temp.columns:
+        df_temp['station'] = df_temp['station'].astype('category').cat.codes
 
     min_max_dict = {}
     std_mean_dict = {}
@@ -72,8 +83,8 @@ def create_sequences(X, y, split_feature=None, n_steps=24):
 
 
 
-def load_dataset(dataset="beijing_PM2.5"):
-    if dataset=="beijing_PM2.5":
+def load_dataset(dataset="beijing_pm2.5"):
+    if dataset=="beijing_pm2.5":
         dataset_link = 'https://archive.ics.uci.edu/ml/machine-learning-databases/00381/PRSA_data_2010.1.1-2014.12.31.csv'
         df = pd.read_csv(dataset_link)
 
@@ -89,41 +100,39 @@ def load_dataset(dataset="beijing_PM2.5"):
             if 'csv' in file:
                 df_ = pd.read_csv(zipfile.open(file))
                 df_list.append(df_)
-    
+
         df = df_list[0]
 
     return df
 
 
-if __name__ == "__main--":
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
 
-    if len(sys.argv) >= 2:
-        print("Usage: python myscript.py arg1 arg2")
-        sys.exit(1)
+  parser.add_argument("dataset", type = str, default = "synthetic")
+  parser.add_argument("scalar", type = str, default = "normalize")
+  parser.add_argument("window_size", type = int, default = 24)
+  args = parser.parse_args()
 
-    dataset = str(sys.argv[0])
-    scalar = str(sys.argv[1])
-    print(dataset)
-    print(scalar)
-    df = load_dataset(dataset)
-    if scalar =="normalise":
+
+  df = load_dataset(args.dataset)
+  if args.scalar =="normalize":
         normalize = True
-        standaize =False
+        standardize = False
 
-    if scalar == "standardize":
+  elif args.scalar == "standardize":
         normalize = False
         standardize = True
 
-    df_temp, _, _ = preprocess_data(df, normalize, standardize)
-    X = df_temp.drop(['PM2.5'], axis=1)
-    y = df_temp['PM2.5']
+  df_temp, _, _ = preprocess_data(df, normalize, standardize)
+  X = df_temp.drop(['pm2.5'], axis=1)
+  y = df_temp['pm2.5']
 
-    data = create_sequences(X, y, 'station')
+  data = create_sequences(X, y, n_steps = args.window_size)
 
 
-    with open("data.dill", 'wb') as f:
-        dill.dump(data, f)
-
+  with open("data.dill", 'wb') as f:
+      dill.dump(data, f)
 
 
 
