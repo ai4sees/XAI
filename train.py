3ae2c99526c47acc28ff76e1856679f3ea8dbd8e
ahmednadiyah1 <104296674+ahmednadiyah1@users.noreply.github.com>
2024-02-15 00:38:34 +0530

    Update train.py
    updated train.py

--- train.py
+++ train.py
@@ -6,14 +6,13 @@ import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
 from statistics import mean
-from keras import layers, Sequential, Model
 from IPython.display import clear_output, display
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, TensorDataset
-
+import argparse
 
 num_epochs = []
 #CREATE LOSS CURVES
@@ -25,17 +24,13 @@ def draw_curve(epochs, train_loss, val_loss):
   plt.show()
 
 
-
-with open("data.dill", "rb") as f:
-   df = dill.load(f)
-
-
-#taking first 10000 windows for training
-X = df[0][:10000]
-y = df[1][:10000]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=13)
-
+def save_loss_curves(epochs, train_loss, val_loss, filename, ylim = False):
+  plt.plot(range(epochs), train_loss, label = "train loss")
+  plt.plot(range(epochs), val_loss, label = "val loss")
+  if ylim:
+    plt.ylim(0, 1)
+  plt.legend()
+  plt.savefig(f"{filename}.png", format = "png")
 
 #FUNCTION FOR MODEL TRAINING
 def run_model(model, x_train, y_train, x_test, y_test, epochs, CNN, device):
@@ -138,31 +133,6 @@ class RNNModel(nn.Module):
 
 
 
-#Train RNN Model
-device= 'cuda' if torch.cuda.is_available() else 'cpu'
-
-model=RNNModel((X.shape[1], X.shape[2]))
-model.to(device)
-model_, train_loss, val_loss = run_model(model, X_train, y_train, X_test, y_test, 100, False, device)
-x_test=torch.from_numpy(X_test).float().to(device)
-y_test_=torch.from_numpy(y_test).float().to(device)
-dataset = TensorDataset(x_test, y_test_)
-
-data_loader = DataLoader(dataset, batch_size=32, shuffle=True)
-
-for x_test, y_test_ in data_loader:
-  predictions= model_(x_test)
-  mse=torch.mean(torch.square(predictions-y_test_))
-
-print("MSE: ", mse)
-
-#SAVE RNN MODEL
-torch.save(model_.state_dict(), "rnn_model")
-
-
-
-
-
 #Transformer model
 class TransformerRegressor(nn.Module):
     def __init__(self, input_dim, d_model, nhead, num_encoder_layers, dim_feedforward, output_dim):
@@ -194,93 +164,85 @@ class TransformerRegressor(nn.Module):
 
 
 
-num_epochs = []
-# Model parameters
-d_model = 128
-nhead = 8
-num_encoder_layers = 3
-dim_feedforward = 512
-output_dim = 1
-input_dim = X.shape[2]
-device= 'cuda' if torch.cuda.is_available() else 'cpu'
-
-
-
-#TRAIN TRANSFORMERS MODEL 
-
-# Create the model
-model = TransformerRegressor(input_dim, d_model, nhead, num_encoder_layers, dim_feedforward, output_dim)
-model.to(device)
-model_, train_loss, val_loss = run_model(model, X_train, y_train, X_test, y_test, 100, False, device)
-x_test=torch.from_numpy(X_test).float().to(device)
-y_test_=torch.from_numpy(y_test).float().to(device)
-dataset = TensorDataset(x_test, y_test_)
-
-data_loader = DataLoader(dataset, batch_size=32, shuffle=True)
-
-for x_test, y_test_ in data_loader:
-  predictions= model_(x_test)
-  mse=torch.mean(torch.square(predictions-y_test_))
-
-print("MSE: ", mse)
-
-
-#SAVE RNN MODEL
-torch.save(model_.state_dict(), "transformer_model")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-with open('beijing_air_processed', 'rb') as f:
-    data = dill.load(f)
-
+if __name__ =="__main__":
+  with open("data.dill", "rb") as f:
+    df = dill.load(f)
+  device= 'cuda' if torch.cuda.is_available() else 'cpu'
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument("--epochs", type = int, default = 100)
+  parser.add_argument("--test_size", type = float, default = 0.2)
+  parser.add_argument("--windows", type = int, default = 1000)
+  args = parser.parse_args()
+
+  #taking first 10000 windows for training
+  X = df[0][:args.windows]
+  y = df[1][:args.windows]
+  epochs = args.epochs
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=args.test_size, random_state=13)
+
+
+  #Train RNN Model
+  model=RNNModel((X.shape[1], X.shape[2]))
+  model.to(device)
+  model_, train_loss, val_loss = run_model(model, X_train, y_train, X_test, y_test, epochs, False, device)
+  x_test=torch.from_numpy(X_test).float().to(device)
+  y_test_=torch.from_numpy(y_test).float().to(device)
+  dataset = TensorDataset(x_test, y_test_)
+
+  data_loader = DataLoader(dataset, batch_size=32, shuffle=True)
+  mse_ = nn.MSELoss()
+  for x_test, y_test_ in data_loader:
+    predictions= model_(x_test)
+    mse = mse_(predictions, y_test_)
+
+  print("MSE: ", mse)
+
+  #SAVE RNN MODEL
+  torch.save(model_.state_dict(), "rnn_model.pt")
+  save_loss_curves(epochs, train_loss, val_loss, "rnn_loss_curves")
+
+
+
+
+
+
+
+  #TRAIN TRANSFORMERS MODEL 
+  
+  #model parameters
+  num_epochs = []
+  # Model parameters
+  parser.add_argument("--d_model", type = int, default = 128)
+  parser.add_argument("--nhead", type = int, default = 8)
+  parser.add_argument("--num_encoder_lay", type = int, default = 3)
+  parser.add_argument("--dim_feedforward", type = int, default = 512)
+  args = parser.parse_args()
+  d_model = args.d_model
+  nhead = args.nhead
+  num_encoder_layers = args.num_encoder_lay
+  dim_feedforward = args.dim_feedforward
+  output_dim = 1
+  input_dim = X.shape[2]
+
+
+  # Create the model
+  model = TransformerRegressor(input_dim, d_model, nhead, num_encoder_layers, dim_feedforward, output_dim)
+  model.to(device)
+  model_, train_loss, val_loss = run_model(model, X_train, y_train, X_test, y_test, epochs, False, device)
+  x_test=torch.from_numpy(X_test).float().to(device)
+  y_test_=torch.from_numpy(y_test).float().to(device)
+  dataset = TensorDataset(x_test, y_test_)
+
+  data_loader = DataLoader(dataset, batch_size=32, shuffle=True)
+  mse_ = nn.MSELoss()
+  for x_test, y_test_ in data_loader:
+    predictions= model_(x_test)
+    mse = mse_(predictions, y_test_)
+
+  print("MSE: ", mse)
+
+
+  #SAVE RNN MODEL
+  torch.save(model_.state_dict(), "transformer_model.pt")
+  save_loss_curves(epochs, train_loss, val_loss, "trans_loss_curves", ylim = True)
