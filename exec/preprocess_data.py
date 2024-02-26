8603676b0b752b25f21fb3a2680ba46fecd14d26
ahmednadiyah1 <104296674+ahmednadiyah1@users.noreply.github.com>
2024-02-26 10:56:41 +0530

    updated names

---
+++ exec/preprocess_data.py
@@ -0,0 +1,32 @@
+import argparse
+from src.utils import load_dataset
+from src.preprocess import Preprocessor
+import dill
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--dataset", type=str, default="synthetic_data")
+parser.add_argument("--url", type=str, default=None)
+parser.add_argument("--y_column", type=str, default='g')
+parser.add_argument("--drop_col", nargs='+', default=None)
+parser.add_argument("--scaler", type=str, default="normalize")
+parser.add_argument("--window_size", type=int, default=10)
+args = parser.parse_args()
+
+df = load_dataset(dataset=args.dataset, url=args.url)
+data_processor = Preprocessor(df, args.y_column)
+data_processor.clean_data(col=args.drop_col)
+if args.scaler == "normalize":
+    _ = data_processor.normalize()
+
+if args.scaler == "standardize":
+    _ = data_processor.standardize()
+
+X, y = data_processor.create_sequences(n_steps=args.window_size)
+print(X.shape)
+print(y.shape)
+data = (X, y)
+df_ = data_processor.get_df()
+print(df_.head())
+with open("data.dill", "wb") as f:
+    dill.dump(data, f)
